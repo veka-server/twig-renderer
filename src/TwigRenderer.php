d79a4034c416852fb277633a1472e07d073b05d1
Nicolas VANHAEZEBROUCK <nvanhaezebrouck@CASTELIS.COM>
2017-11-06 22:55:02 +0100

    init

---
+++ src/TwigRenderer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace VekaServer\TwigRenderer;
+
+use VekaServer\Interfaces\RendererInterface;
+
+class TwigRenderer implements RendererInterface
+{
+
+    private $twig;
+
+    /**
+     * constructor.
+     * @param string $path_to_templates
+     * @param null|string $path_to_cache
+     */
+    public function __construct(string $path_to_templates, ?string $path_to_cache = null)
+    {
+        $loader = new \Twig_Loader_Filesystem($path_to_templates);
+        $this->twig = new \Twig_Environment($loader, array(
+            'cache' => $path_to_cache,
+        ));
+    }
+
+    /**
+     * @param string $templatePath
+     * @param array|null $data
+     * @return string
+     */
+    public function render(string $templatePath, array $data = array()): string
+    {
+        return $this->twig->render($templatePath, $data);
+    }
+}
